ea2792bcbd47ae50f15d65b0da5a271dcdb613ce
Nnn <Nnn@i.ua>
2016-11-09 14:50:39 +0200

    Create project by tutorial

---
+++ blog/app/Http/Controllers/TaskController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
+
+class TaskController extends Controller {
+
+    /**
+     * Экземпляр TaskRepository.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    /**
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks) {
+        $this->middleware('auth');
+        $this->tasks = $tasks;
+    }
+
+    /**
+     * Отображение списка всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
+    }
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * Уничтожить заданную задачу.
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task) {
+        $this->authorize('destroy', $task);
+        $task->delete();
+        return redirect('/tasks');
+    }
+
+}
--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Маршруты аутентификации...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Маршруты регистрации...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+//Маршруты задач
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
